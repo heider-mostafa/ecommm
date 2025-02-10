8306ff7cb8a97a49db03b562ac373edddf594f1e
heider-mostafa <mostafa.heider9@gmail.com>
2025-02-10 13:03:24 -0500

    db connection debugging

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -29,7 +29,9 @@ export const connectToDatabase = async () => {
     cached.promise || 
     mongoose.connect(MONGODB_URL, { 
       dbName: 'imaginify', bufferCommands: false 
+      
     })
+    console.log("connected to db")
 
   cached.conn = await cached.promise;
 
